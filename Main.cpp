07b9c404ddc6869d05ca0b0059358bd14237fee2
Your Name <ahep0ki@gmail.com>
2020-03-02 10:00:47 -0800

    2nd

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 int main()
 {
+  int array[100];
+  int len = sizeof(array)/sizeof(array[0]);
+  cout<<len<<endl;
   ifstream inFile;
-  int heap [100];
-  inFile.open("myFile.exe");
-  inFile >> heap[1] >> heap[2] >> heap[3] >> heap[4];
+  int heap [100] = { 0 };
+  inFile.open("myFile");
+  for (int i = 0; i < 100; i++){
+    inFile >> heap[i];
+    //cout<<heap[i]<<endl;
+  }
   inFile.close();
   cout<<"\n  5"<<endl;
   cout<<" / \\"<<endl;
