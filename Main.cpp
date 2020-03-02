4392a0039dc641fa56b1d69b87a21b0229c067f7
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-02 08:20:09 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,16 @@
 #include <cstring>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+  ifstream inFile;
+  int heap [100];
+  inFile.open("myFile.exe");
+  inFile >> heap[1] >> heap[2] >> heap[3] >> heap[4];
+  inFile.close();
   cout<<"\n  5"<<endl;
   cout<<" / \\"<<endl;
   cout<<"5   5"<<endl;
