7963e911e8da6cb57f2d683bcbaf9137cc25d664
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-04 09:11:24 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
+void sort(int[], int);
+void printTree(int[], int);
+
 int main()
 {
   ifstream inFile;
   int heap [100] = { 0 };
   int len = sizeof(heap)/sizeof(heap[0]);
+  //Sort
+  //Print tree
   inFile.open("myFile");
   for (int i = 0; i < len; i++){
     inFile >> heap[i];
@@ -17,4 +22,12 @@ int main()
   cout<<endl;
   inFile.close();
   return 0;
+}
+
+void sort(int heap[], int len){
+  
+}
+
+void printTree(int heap[], int len){
+  
 }
